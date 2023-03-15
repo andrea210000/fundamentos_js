3f2cccae92ea49bd36af5389a8ca2d49e452e49c
andrea210000 <118681659+andrea210000@users.noreply.github.com>
2023-03-14 21:53:15 -0500

    Add files via upload

---
+++ js/fundamentos_js/app.js
@@ -0,0 +1,19 @@
+
+
+
+let a = 10; //esta es una variable, a significa la variable y 10 significa el valor que tiene la variable a y se le asigna el valor //
+  b = 20;
+  c = 'hola ';
+  d = 'spiderman ';
+  x = a + b;
+ const hola = c + d;
+ // c = 20; no se puede hacer por que va a saltar un error//
+//let a = 10, b = 10, c = 10, d = 10, x = a + b; //esta es una forma de escribir el codigo mas corto//
+//console.warn(b); //mensaje que se muestra en la consola como una advertencia
+//console.error(c); //mensaje de consola que muestra el contenido como si fuera un error pero no lo es//
+console.log('%c Mis variables','color:pink; font-weigth bold'); //se le asigna caracteristicas de personalizacion/
+//console.log({a}); // para saber la variable se pone la variable se pone entre comillas simples o llaves
+//console.log({b});//console= al lugar donde se muestra= objeto
+//console.log({c});//log= en la forma que se muestra
+//console.log({x}); //dice que muestre la variable en la consola//
+//console.table({a,b,c,d,x})
